2f013b81a936faf71c9a2f58f767abfcf8ead692
tludlow99 <twjludlow@gmail.com>
2020-02-26 06:16:40 +0000

    Starting work on the create-trade page

--- src/containers/Trading/Forms/CreateTrade.js
+++ src/containers/Trading/Forms/CreateTrade.js
@@ -1,14 +1,68 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
-//AutoSuggestion inputs
-import CompanyAutoSuggest from "./AutoSuggestions/CompanyAutoSuggest";
-
+import api from "../../../api.js";
+import axios from "axios";
 
 export default function CreateTrade() {
     //Data for the auto suggested form inputs is found through the redux state of the app.
-    const [buyingCompany, setBuyingCompany] = useState('');
+    
+    const [loading, setLoading] = useState(true);
+    const [companies, setCompanies] = useState([]);
+    const [products, setProducts] = useState([]);
+    const [currencies, setCurrencies] = useState([]);
+    const [error, setError] = useState("");
 
+    useEffect(()=> {
+        getCompaniesAndCurrenciesAndProducts();
+    }, []);
 
+    const getCompaniesAndCurrenciesAndProducts = () => {
+        setLoading(true);
+        axios.all([
+            api.get("/company/list"),
+            api.get("/currency/list"),
+            api.get("/product/list")
+        ]).then(axios.spread((companyRes, currencyRes, productRes) => {
+            console.log(companyRes, currencyRes, productRes);
+            setLoading(false);
+            setCompanies(companyRes.data);
+            setCurrencies(currencyRes.data);
+
+            //Set the products to also include stocks at the top, this is a viable option not from the database
+            setProducts([{id: -1, name: "Stocks"}, ...productRes.data]);
+            
+        })).catch(err => {
+            setError(err.message);
+            setLoading(false);
+            console.log(err);
+        });
+
+    }
+
+    if (loading || error) {
+        return (
+            <>
+            <div className="flex flex-col items-center md:items-start">
+                <h2 className="text-brand font-bold text-xl">Create a new trade</h2>
+                <p>Insert the details for a derivative manually</p>
+                <hr className="my-2" />
+                <p className="text-center">If you choose to have a the trade type be a stock, the buying company is the stock you are buying.</p>
+            </div>
+            { error && 
+                <div className="">
+                    <h4 className="text-red-700 font-bold text-xl">Error</h4>
+                    <p>{error}</p>
+                </div>
+            }
+
+            {loading && <div className="h-32 w-32 mx-auto spinner text-center"></div> }
+            </>
+        );
+    }
+
+    //General render
     return (
         <>
         <div className="flex flex-col items-center md:items-start">
@@ -16,17 +70,17 @@ export default function CreateTrade() {
             <p>Insert the details for a derivative manually</p>
         </div>
 
-        <form className="mt-8 w-10/12 mx-auto h-auto flex bg-white shadow rounded-lg">
-            <div className="mx-auto py-8 px-2">
-                <div className="mb-4">
-                <label className="block text-gray-700 text-md font-bold mb-2" htmlFor="buyingCompany">
-                    Buying Company
-                    <span className="block text-gray-700 text-sm font-normal">The company which is buying the derivative, can either be the name or their ID</span>
-                </label>
-                <CompanyAutoSuggest />
-                {/* <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="type" type="text" autoComplete="on" placeholder="Stocks" /> */}
-                </div>
-            </div>
+        <form className="mt-8 w-11/12 p-4 mx-auto h-auto flex flex-col items-center bg-white shadow rounded-lg">
+                <label className="mb-4 text-brand text-md font-semibold" htmlFor="product-dropdown">Product Type</label>
+                <Autocomplete
+                    id="combo-box-demo"
+                    name="product-dropdown"
+                    options={products}
+                    getOptionLabel={option => option.name}
+                    style={{ width: 300 }}
+                    renderInput={params => <TextField {...params} label="Product" variant="outlined" />}
+                />
+                <button className="mx-auto mt-2 text-center px-3 py-2 rounded shadow bg-brand text-white uppercase font-semibold text-sm" type="submit">Create Trade</button>
         </form>
         </>
     );
---
+++ src/containers/Trading/Forms/DeleteTrade.js
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+
+import api from "../../../api.js";
+
+
+export default function DeleteTrade() {
+
+    const [loading, setLoading] = useState(false);
+    const [tradeID, setTradeID] = useState("");
+    const [trade, setTrade] = useState([]);
+    const [error, setError] = useState("");
+    const [justDeleted, setJustDeleted] = useState(false);
+
+    const submit = e => {
+        e.preventDefault();
+        setLoading(true);
+        console.log("Form submitted", tradeID);
+        getTrade(tradeID);
+    }
+
+    const getTrade = id => {
+        api.get("/trade/id=" + id).then(response => {
+            setError("");
+            setLoading(false);
+            setJustDeleted(false);
+            if (response.data.length === 0) {
+                setError("No trades exist with that ID")
+            } else {
+                setTrade(response.data[0]);
+                console.log(response);
+            }
+            
+        }).catch(err => {
+            console.log(err);
+            setLoading(false);
+            setError(err.message);
+        });
+    }
+
+    const deleteTrade = id => {
+        setLoading(true);
+        api.post("/trade/delete",
+        {
+            trade_id: id
+        }).then(response => {
+            console.log(response);
+            setLoading(false);
+            setJustDeleted(true);
+        }).catch(err => {
+            console.log(err);
+            setError(err.message);
+            setLoading(false);
+        });
+    }
+
+    return (
+        <>
+        {error.length > 0 &&  
+            <>
+                <p className="text-red-800 font-bold text-xl">Error</p>
+                <p className="text-red-600">{error}</p>
+            </>
+        }
+        
+        {justDeleted && <p className="text-green-600 font-semibold mb-4">Successfully deleted trade: {tradeID} </p>}
+        <h2 className="text-brand text-2xl font-bold">Delete a Trade</h2>
+        <p>Search for the ID of a trade you wish to delete</p>
+
+        {loading === false ? 
+        <form className="mt-6" onSubmit={submit}>
+            <label className="block uppercase tracking-wide text-gray-600 text-xs font-bold mb-2" htmlFor="trade-id">
+                Trade ID
+            </label>
+            <input className="appearance-none block w-full md:w-1/6 bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3" value={tradeID} onChange={e => setTradeID(e.target.value)}id="trade-id" type="text" placeholder="################" />
+            <input className="bg-brand text-white px-3 py-2 rounded hover:shadow hover:cursor-pointer text-xs uppercase tracking-wide" type="submit" value={loading ? "Loading..." : "Search"} />
+        </form> : ""}
+
+        {loading ? <div className="h-32 w-32 mx-auto spinner text-center"></div> : ""}
+        
+        {error.length === 0 && !(trade instanceof Array) && !justDeleted ?
+            <div className="w-8/12 mt-8 py-4 px-4 text-center rounded shadow bg-white">
+                <p><span className="font-bold">Trade ID</span>: {trade.id}</p>
+                <p><span className="font-bold">Product ID</span>: {trade.product}</p>
+                <p><span className="font-bold">Date</span>: {trade.date}</p>
+                <p><span className="font-bold">Maturity Date</span>: {trade.maturity_date}</p>
+                <p><span className="font-bold">Strike Price</span>: {trade.strike_price}</p>
+                <p><span className="font-bold">Quantity</span>: {trade.quantity}</p>
+                <p><span className="font-bold">Buying Party</span>: {trade.buying_party}</p>
+                <p><span className="font-bold">Selling Party</span>: {trade.selling_party}</p>
+                <p><span className="font-bold">Notional Currency</span>: {trade.notional_currency}</p>
+                <p><span className="font-bold">Underlying Currency</span>: {trade.underlying_currency}</p>
+
+                <div className="mt-6 w-full mx-auto flex justify-center">
+                    <button onClick={()=> deleteTrade(trade.id)} className="ml-2 px-4 py-1 text-white rounded shadow bg-red-700 hover:cursor-pointer hover:bg-red-500">Delete</button>
+                </div>
+            </div>
+        : ""}
+        </>
+    );
+}
