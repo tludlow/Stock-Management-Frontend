7efb76515891dac9b6c83871c2bce7894ee9c457
tludlow99 <twjludlow@gmail.com>
2020-03-03 21:53:57 +0000

    Reports page

--- src/containers/Reports/Report.js
+++ src/containers/Reports/Report.js
@@ -30,6 +30,10 @@ export default function Report(props) {
         })
     }, [])
 
+    const prettyifyAttribute = (name) => {
+
+    }
+
     if(error.length > 0) {
         return (
             <>
@@ -64,15 +68,12 @@ export default function Report(props) {
                                     }
                             </PDFDownloadLink>
                         </div>
-                        <p>Trading Summary (Click on a section title to jump to that section)</p>
+                        <p className="mt-4 md:mt-0">Trading Summary (Click on a section title to jump to that section)</p>
                         <div className="mt-3">
-                            <ul className="flex flex-col md:flex-row justify-between">
-                                <li className="mt-2"><a className="px-4 py-1 bg-brand text-white rounded-full" href="#new-trades">New Trades: <span>{reportData.num_of_new_trades}</span></a></li>
-                                <li className="mt-2"><a className="px-4 py-1 bg-brand text-white rounded-full" href="#edited-trades">Edited Trades: <span>{reportData.num_of_edited_trades}</span></a></li>
-                                <li className="mt-2"><a className="px-4 py-1 bg-brand text-white rounded-full" href="#deleted-trades">Deleted Trades: <span>{reportData.num_of_deleted_trades}</span></a></li>
-                                <li className="mt-2"><a className="px-4 py-1 bg-brand text-white rounded-full" href="#erroneous-trades">Erroneous Trades: <span>{reportData.erroneousTradeCount}</span></a></li>
-                                <li className="mt-2"><a className="px-4 py-1 bg-brand text-white rounded-full" href="#user-corrected-trades">User Corrected Trades: <span>{reportData.userCorrectionsCount}</span></a></li>
-                                <li className="mt-2"><a className="px-4 py-1 bg-brand text-white rounded-full" href="#system-corrected-trades">System Corrected Trades: <span>{reportData.systemCorrectionsCount}</span></a></li>
+                            <ul className="flex flex-col md:flex-row">
+                                <li className="mt-2 mr-6"><a className="px-4 py-1 bg-brand text-white rounded-full" href="#new-trades">New Trades: <span>{reportData.num_of_new_trades}</span></a></li>
+                                <li className="mt-2 mr-6"><a className="px-4 py-1 bg-brand text-white rounded-full" href="#edited-trades">Edited Trades: <span>{reportData.num_of_edited_trades}</span></a></li>
+                                <li className="mt-2 mr-6"><a className="px-4 py-1 bg-brand text-white rounded-full" href="#deleted-trades">Deleted Trades: <span>{reportData.num_of_deleted_trades}</span></a></li>
                             </ul>
                         </div>
                     </div>
@@ -84,7 +85,7 @@ export default function Report(props) {
                         
                         {reportData.num_of_new_trades > 0 &&
                         <div className="mt-3">
-                            <table className="table-fixed rounded">
+                            <table className="table-fixed rounded shadow">
                                 <thead>
                                     <tr>
                                         <th className="w-1/12 px-4 py-2">ID</th>
@@ -96,15 +97,14 @@ export default function Report(props) {
                                         <th className="w-1-12 px-4 py-2">Strike Price</th>
                                         <th className="w-1-12 px-4 py-2">Currencies <small className="text-xs font-normal">(Underlying) | (Notional)</small></th>
                                         <th className="w-1-12 px-4 py-2">Maturity Date</th>
-                                        <th className="w-1/12 px-4 py-2">Date</th>
+                                        <th className="w-1/12 px-4 py-2">Created</th>
                                         <th className="w-1/12 px-4 py-2">Actions</th>
                                     </tr>
                                 </thead>
 
                                 <tbody>
                                     {reportData.created_trades.map((created_trade, index)=> (
-                                        index % 2 === 0 ?
-                                        <tr>
+                                        <tr className="bg-white">
                                             <td className="border px-4 py-2 text-center">{created_trade.id}</td>
                                             <td className="border px-4 py-2 text-center">{created_trade.buying_party}</td>
                                             <td className="border px-4 py-2 text-center">{created_trade.selling_party}</td>
@@ -112,25 +112,8 @@ export default function Report(props) {
                                             <td className="border px-4 py-2 text-center">{created_trade.underlying_price}</td>
                                             <td className="border px-4 py-2 text-center">{created_trade.quantity}</td>
                                             <td className="border px-4 py-2 text-center">{created_trade.strike_price}</td>
-                                            <td  className="border px-4 py-2 text-center">{created_trade.underlying_currency} | {created_trade.notional_currency}</td>
-                                            <td className="border px-4 py-2 text-center">{moment(created_trade.maturity_date).fromNow()}</td>
-                                            <td className="border px-4 py-2 text-center">{moment(created_trade.date).fromNow()}</td>
-                                            <td className="border px-4 py-2">
-                                                <Link className="mr-3 px-2 py-1 bg-blue-700 text-white rounded" to={`/trading/edit-trade/${created_trade.id}`}>Edit</Link>
-                                                <Link className="ml-3 px-2 py-1 bg-red-700 text-white rounded" to={`/trading/delete-trade/${created_trade.id}`}>Delete</Link>
-                                            </td>
-                                        </tr>
-                                        :
-                                        <tr className="bg-gray-100">
-                                            <td className="border px-4 py-2 text-center">{created_trade.id}</td>
-                                            <td className="border px-4 py-2 text-center">{created_trade.buying_party}</td>
-                                            <td className="border px-4 py-2 text-center">{created_trade.selling_party}</td>
-                                            <td className="border px-4 py-2 text-center">{created_trade.product}</td>
-                                            <td className="border px-4 py-2 text-center">{created_trade.underlying_price}</td>
-                                            <td className="border px-4 py-2 text-center">{created_trade.quantity}</td>
-                                            <td className="border px-4 py-2 text-center">{created_trade.strike_price}</td>
-                                            <td  className="border px-4 py-2 text-center">{created_trade.underlying_currency} | {created_trade.notional_currency}</td>
-                                            <td className="border px-4 py-2 text-center">{moment(created_trade.maturity_date).fromNow()}</td>
+                                            <td className="border px-4 py-2 text-center">{created_trade.underlying_currency} | {created_trade.notional_currency}</td>
+                                            <td className="border px-4 py-2 text-center">{created_trade.maturity_date}</td>
                                             <td className="border px-4 py-2 text-center">{moment(created_trade.date).fromNow()}</td>
                                             <td className="border px-4 py-2">
                                                 <Link className="mr-3 px-2 py-1 bg-blue-700 text-white rounded" to={`/trading/edit-trade/${created_trade.id}`}>Edit</Link>
@@ -149,12 +132,21 @@ export default function Report(props) {
                         <p>Trades which the user has edited on this day, with a description of each edit</p>
 
                         <div className="mt-3">
-                            <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
+                            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                                 {reportData.edited_trades.map((edited_trade, index)=> (
-                                    <div className="bg-white rounded shadow p-4 mb-3" key={index}>
+                                    <div className="bg-white rounded shadow p-4 mb-3 flex justify-around" key={index}>
                                         <div className="">
                                             <h5 className="text-brand font-bold">{edited_trade.trade.id}</h5>
-                                            <p>Edits: {edited_trade.num_of_edits}</p>
+                                            <p className="mb-2">Edits: {edited_trade.num_of_edits}</p>
+                                            <Link className="px-3 py-1 rounded bg-gray-500 text-white" to={`/trade/${edited_trade.trade.id}`}>View</Link>
+                                        </div>
+                                        <div className="pl-4 border-l-2 border-dashed border-gray-600">
+                                            {edited_trade.edits.map((edit, idx)=> (
+                                                <div className="mb-2" key={idx}>
+                                                    <p>({idx+1}) {prettyifyAttribute(edit.attribute_edited)}</p>
+                                                    <p><span className="text-red-600 line-through">{edit.old_value}</span> ->  <span className="text-green-700 font-semibold">{edit.new_value}</span></p>
+                                                </div>
+                                            ))}
                                         </div>
                                     </div>
                                 ))}
@@ -165,21 +157,47 @@ export default function Report(props) {
                     <section className="mt-12" id="deleted-trades">
                         <h3 className="text-brand font-semibold text-lg">Deleted Trades</h3>
                         <p>Trades which a user has deleted on this daye</p>
-                    </section>
-    
-                    <section className="mt-12" id="erroneous-trades">
-                        <h3 className="text-brand font-semibold text-lg">Erroneous Trades</h3>
-                        <p>Trades identified to have erroneous data</p>
-                    </section>
-    
-                    <section className="mt-12" id="user-corrected-trades">
-                        <h3 className="text-brand font-semibold text-lg">User Corrections</h3>
-                        <p>Trades which have bheen identified to be erroneous by the system and the user has edited them</p>
-                    </section>
-    
-                    <section className="mt-12" id="system-corrected-trades">
-                        <h3 className="text-brand font-semibold text-lg">System Corrections</h3>
-                        <p>Trades which have erroneous data which the system has identified and corrected automatically</p>
+
+                        {reportData.num_of_deleted_trades > 0 &&
+                        <div className="mt-3">
+                            <table className="table-fixed rounded shadow">
+                                <thead>
+                                    <tr>
+                                        <th className="w-1/12 px-4 py-2">ID</th>
+                                        <th className="w-1/12 px-4 py-2">Buying Party</th>
+                                        <th className="w-1/12 px-4 py-2">Selling Party</th>
+                                        <th className="w-1-12 px-4 py-2">Product</th>
+                                        <th className="w-1-12 px-4 py-2">Quantity</th>
+                                        <th className="w-1-12 px-4 py-2">Underlying Price</th>
+                                        <th className="w-1-12 px-4 py-2">Strike Price</th>
+                                        <th className="w-1-12 px-4 py-2">Currencies <small className="text-xs font-normal">(Underlying) | (Notional)</small></th>
+                                        <th className="w-1-12 px-4 py-2">Maturity Date</th>
+                                        <th className="w-1/12 px-4 py-2">Created</th>
+                                        <th className="w-1/12 px-4 py-2">Deleted At</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    {reportData.deleted_trades.map((deleted_trade, index)=> (
+                                        <tr className="bg-white">
+                                            <td className="border px-4 py-2 text-center">{deleted_trade.trade.id}</td>
+                                            <td className="border px-4 py-2 text-center">{deleted_trade.trade.buying_party}</td>
+                                            <td className="border px-4 py-2 text-center">{deleted_trade.trade.selling_party}</td>
+                                            <td className="border px-4 py-2 text-center">{deleted_trade.trade.product}</td>
+                                            <td className="border px-4 py-2 text-center">{deleted_trade.trade.underlying_price}</td>
+                                            <td className="border px-4 py-2 text-center">{deleted_trade.trade.quantity}</td>
+                                            <td className="border px-4 py-2 text-center">{deleted_trade.trade.strike_price}</td>
+                                            <td className="border px-4 py-2 text-center">{deleted_trade.trade.underlying_currency} | {deleted_trade.trade.notional_currency}</td>
+                                            <td className="border px-4 py-2 text-center">{deleted_trade.trade.maturity_date}</td>
+                                            <td className="border px-4 py-2 text-center">{moment(deleted_trade.trade.date).fromNow()}</td>
+                                            <td className="border px-4 py-2 text-center">{moment(deleted_trade.deletion[0].deleted_at).fromNow()}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                        
+                        }
                     </section>
             </div>
         );
